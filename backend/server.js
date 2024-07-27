84709cd8a9857dc4d45faa6f353bc7dd0cd3782c
giuRa13 <giulioravazzolobass@gmail.com>
2024-07-27 02:52:16 +0200

    middleware, routes, Signup

--- backend/server.js
+++ backend/server.js
@@ -2,6 +2,9 @@
 import express from "express";
 import dotenv from "dotenv";
 import connectDB from "./db/connectDB.js";
+import cookieParser from "cookie-parser";
+import userRoutes from "./routes/userRoutes.js";
+
 
 dotenv.config();
 
@@ -10,5 +13,13 @@ connectDB();
 const app = express();
 
 const PORT = process.env.PORT || 5000;
+
+// (built-in) Middleware
+app.use(express.json()); // to parse JSON data in the req.body 
+app.use(express.urlencoded({extended: true})) // even if the req.body has some nested obj, it will be able to parse without problem
+app.use(cookieParser()); // allows to get the cookie from req and set the cookie in the response
+
+// Routes
+app.use("/api/users", userRoutes);
 
 app.listen(PORT, () => console.log(`Server running at http://localhost:${PORT}`));
