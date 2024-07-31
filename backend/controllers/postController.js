24ae1019a77da3e7d8cf057860663251094e7953
giuRa13 <giulioravazzolobass@gmail.com>
2024-07-31 03:07:42 +0200

    like/unlike post

--- backend/controllers/postController.js
+++ backend/controllers/postController.js
@@ -61,7 +61,34 @@ const deletePost = async (req, res) => {
         res.status(500).json({message: error.message});
         console.log("Error in deletePost: ", error.message);
     }
-
 };
 
-export {createPost, getPost, deletePost}
+
+
+const likeUnlikePost = async (req, res) => {
+    try {  
+        const {id:postId} = req.params; 
+        const userId = req.user._id;
+
+        const post = await Post.findById(postId);
+        if(!post) return res.status(404).json({message: "Post not found"});
+
+        const userLikedPost = post.likes.includes(userId);
+
+        if(userLikedPost){
+            // Unlike post
+            await Post.updateOne({ _id: postId }, {$pull: { likes: userId }});
+            res.status(200).json({message: "Post unliked successfully"});
+        } else {
+            post.likes.push(userId);
+            await post.save();
+            res.status(200).json({message: "Post liked successfully"});
+        }
+    } catch (error) {
+        res.status(500).json({message: error.message});
+        console.log("Error in likeUnlikePost: ", error.message);
+    }
+};
+
+
+export {createPost, getPost, deletePost, likeUnlikePost}
--- backend/routes/postRoutes.js
+++ backend/routes/postRoutes.js
@@ -1,11 +1,12 @@
 import express from "express";
-import { createPost, deletePost, getPost } from "../controllers/postController.js";
+import { createPost, deletePost, getPost, likeUnlikePost } from "../controllers/postController.js";
 import protectRouteMiddleware from "../middlewares/protectRoute.js";
 
 const router = express.Router();
 
 router.get("/:id", getPost);
 router.post("/create", protectRouteMiddleware, createPost);
-router.delete("/:id",protectRouteMiddleware ,deletePost)
+router.delete("/:id", protectRouteMiddleware, deletePost);
+router.put("/like/:id", protectRouteMiddleware, likeUnlikePost);
 
 export default router;
