72cf1507bfe1e073efe36eec6ada88187de4c8f3
giuRa13 <giulioravazzolobass@gmail.com>
2024-07-31 20:22:42 +0200

    reply to post, get feed posts

--- backend/controllers/postController.js
+++ backend/controllers/postController.js
@@ -34,13 +34,12 @@ const getPost = async (req, res) => {
 
         if(!post) return res.status(404).json({message: "Post not found"});
 
-        res.status(200).json({ post });
+        res.status(200).json( post );
 
     } catch (error) {
         res.status(500).json({message: error.message});
         console.log("Error in getPost : ", error.message);
     }
-
 }; 
 
 
@@ -91,4 +90,48 @@ const likeUnlikePost = async (req, res) => {
 };
 
 
-export {createPost, getPost, deletePost, likeUnlikePost}
+
+const replyToPost = async (req, res) => {
+    try {
+        const {text} = req.body;
+        const postId = req.params.id;
+        const userId = req.user._id;
+        const userProfilePic = req.user.profilePic;
+        const username = req.user.username;
+
+        if(!text) return res.status(400).json({message: "Text field is required"});
+    
+        const post = await Post.findById(postId);
+        if(!post) return res.status(404).json({message: "Post not found"});
+
+        const reply = {userId, text, userProfilePic, username};
+        post.replies.push(reply);
+        await post.save();
+        res.status(200).json({message: "Replay added successfully", post});
+        
+    } catch (error) {
+        res.status(500).json({message: error.message});
+        console.log("Error in replayToPost: ", error.message);
+    }
+};
+
+
+
+const getFeedPost = async (req, res) =>{
+    try {
+        const userId = req.user._id;
+        const user = await User.findById(userId);
+
+        if(!user) return res.status(404).json({message: "User not found"});
+
+        const following = user.following;
+        const feedPosts = await Post.find({postedBy: {$in:following}}).sort({createdAt: -1});
+        res.status(200).json(feedPosts);
+
+    } catch (error) {
+        res.status(500).json({message: error.message});
+        console.log("Error in getFeedPost: ", error.message);   
+    }
+};
+
+export {createPost, getPost, deletePost, likeUnlikePost, replyToPost, getFeedPost}
--- backend/routes/postRoutes.js
+++ backend/routes/postRoutes.js
@@ -1,12 +1,15 @@
 import express from "express";
-import { createPost, deletePost, getPost, likeUnlikePost } from "../controllers/postController.js";
+import { createPost, deletePost, getFeedPost, getPost, likeUnlikePost, replyToPost } from "../controllers/postController.js";
 import protectRouteMiddleware from "../middlewares/protectRoute.js";
 
 const router = express.Router();
 
+router.get("/feed", protectRouteMiddleware, getFeedPost)
 router.get("/:id", getPost);
 router.post("/create", protectRouteMiddleware, createPost);
 router.delete("/:id", protectRouteMiddleware, deletePost);
 router.put("/like/:id", protectRouteMiddleware, likeUnlikePost);
+router.post("/reply/:id", protectRouteMiddleware, replyToPost);
+
 
 export default router;
