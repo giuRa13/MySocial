cefa778fed1360224e269edbdddfc2181022a7d4
giuRa13 <giulioravazzolobass@gmail.com>
2024-08-28 02:51:48 +0200

    display messages

--- src/components/Message.jsx
+++ src/components/Message.jsx
@@ -1,22 +1,29 @@
+import { useRecoilValue } from "recoil";
 import avatarSVG from "../assets/avatar.svg";
+import { selectedConversationsAtom } from "../atoms/messagesAtom";
+import userAtom from "../atoms/userAtom";
 
 
-const Message = ({ownMessage}) => {
+const Message = ({ownMessage, message}) => {
+
+  const selectedConversation = useRecoilValue(selectedConversationsAtom);
+  const user = useRecoilValue(userAtom);
+
   return (
     <>
     {ownMessage ? (
         <div className="flex justify-end my-2 gap-2">
-            <p className="max-w-[300px] bg-greenM1 text-grayM p-2 rounded-lg" id="message-text">Lorem ipsum dolor sit amet consectetur adipisicing elit. Laudantium, qui.</p>
+            <p className="max-w-[300px] bg-greenM1 text-grayM p-2 rounded-lg" id="message-text">{message.text}</p>
             <div className=" avatar online inline-block items-center w-12 h-12 min-w-12 min-h-12 rounded-full border-2 border-greenM1">    
-              <img src={ avatarSVG} alt="user" className="rounded-full w-[100%] h-[100%]"/>
+              <img src={user.profilePic || avatarSVG} alt="user" className="rounded-full w-[100%] h-[100%]"/>
             </div>
         </div>
     ) : (
         <div className="flex justify-start my-2 gap-2">
             <div className=" avatar online inline-block items-center w-12 h-12 min-w-12 min-h-12 rounded-full border-2 border-greenM1">    
-              <img src={ avatarSVG} alt="user" className="rounded-full w-[100%] h-[100%]"/>
+              <img src={selectedConversation.userProfilePic || avatarSVG} alt="user" className="rounded-full w-[100%] h-[100%]"/>
             </div>
-            <p className="max-w-[300px] bg-grayM p-2 rounded-lg" id="message-text">Lorem ipsum dolor sit amet consectetur adipisicing elit. Laudantium, qui dolor sit amet consectetur adipisicing elit. Laudantium, qui </p>
+            <p className="max-w-[300px] bg-grayM p-2 rounded-lg" id="message-text">{message.text}</p>
         </div>
     )}
 
--- src/components/MessageContainer.jsx
+++ src/components/MessageContainer.jsx
@@ -1,35 +1,71 @@
+import { useEffect, useState } from "react";
 import avatarSVG from "../assets/avatar.svg";
 import Message from "./Message";
 import MessageInput from "./MessageInput";
 import MessageSkeleton from "./MessageSkeleton";
+import { toast } from "react-toastify";
+import { useRecoilState, useRecoilValue } from "recoil";
+import { selectedConversationsAtom } from "../atoms/messagesAtom";
+import userAtom from "../atoms/userAtom";
 
 
 const MessageContainer = () => {
+
+  const [selectedConversation, setSelectedConversation] = useRecoilState(selectedConversationsAtom);
+  const [loadingMessages, setLoadingMessages] = useState(true);
+  const [messages, setMessages] = useState([]);
+  const currentUser = useRecoilValue(userAtom);
+
+  useEffect(() => {
+    const getMessages = async () => {
+      setLoadingMessages(true);
+      setMessages([]);
+      try {
+        const res = await fetch(`/api/messages/${selectedConversation.userId}`);
+        const data = await res.json();
+        if(data.error) {
+          toast.error(data.error, {style: { background: "#d6436e", color: '#3c444c'}});
+          return;
+        }
+        setMessages(data);
+
+      } catch (error) {
+        toast.error(error.message, {style: { background: "#d6436e", color: '#3c444c'}});
+      } finally {
+        setLoadingMessages(false);
+      }
+    }
+    getMessages();
+  }, [selectedConversation.userId])
+
+
   return (
     <div className="flex flex-col w-full">
         
         <div className="flex w-full h-12 items-center my-4 gap-2">
             <div className=" avatar online inline-block items-center w-16 h-16 min-w-16 min-h-16 rounded-full border-2 border-greenM1">    
-              <img src={ avatarSVG} alt="user" className="rounded-full w-[100%] h-[100%]"/>
+              <img src={selectedConversation.userProfilePic ||avatarSVG} alt="user" className="rounded-full w-[100%] h-[100%]"/>
             </div>
-            <h2 className="text-lg ml-2 font-semibold">User Full Name</h2>
+            <h2 className="text-lg ml-2 font-semibold">{selectedConversation.name}</h2>
         </div>
         <hr className="mt-4 border-greenM1"/>
 
         <div className="flex flex-col gap-4 my-4 overflow-auto">
-            {false && [...Array(7)].map((_, idx) => <MessageSkeleton key={idx}/>)}
-            
+            {loadingMessages && [...Array(7)].map((_, idx) => <MessageSkeleton key={idx}/>)}
+                      
             <div className="flex flex-col px-2">
-                <Message ownMessage={true}/>
-                <Message ownMessage={false}/>
-                <Message ownMessage={true}/>
-                <Message ownMessage={false}/>
-                <Message ownMessage={false}/>
-                <Message ownMessage={true}/>
-            </div>
+            {!loadingMessages && (
+                messages.map((message) => (
+                  <Message key={message._id} message={message} ownMessage={currentUser._id === message.sender}/>
+                ))
+            )}
+            </div>                        
         </div>
         
-        <MessageInput/>
+        <div className="mt-auto">
+          <MessageInput/>
+        </div>
+        
     </div>
   )
 }
